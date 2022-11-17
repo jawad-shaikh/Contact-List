b48fd5ec6f5def77bac0b5f4ebaa74609ac06c95
jawad-shaikh <shaikhjawad314@gmail.com>
2022-11-17 20:40:11 +0500

    getStaticProps fetching data from json placeholder

--- pages/ninjas/index.jsx
+++ pages/ninjas/index.jsx
@@ -1,7 +1,26 @@
-const Ninjas = () => {
+import styles from "../../styles/Ninjas.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <div>
       <h1>All Ninjas</h1>
+
+      <div>
+        {ninjas.map((ninja) => (
+          <a className={styles.single}>
+            <h3 key={ninja.id}>{ninja.name}</h3>
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
